71c8696a0739de2fc85a69febdb85e20422a0f32
Liam Birkinshaw-Doyle <ldoyle369@gmail.com>
2022-06-22 09:44:20 +0100

    added DOM created header

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './style.css'
+
+const content = document.getElementById('content')
+
+function createHeaderBar() {
+    const header = document.createElement('header');
+    const headerHomeButton = document.createElement('button');
+    headerHomeButton.classList.add('page-button')
+    const headerMenuButton = headerHomeButton.cloneNode('true');
+    const headerAboutButton = headerHomeButton.cloneNode('true');
+
+    headerAboutButton.textContent = 'ABOUT';
+    headerHomeButton.textContent = 'HOME';
+    headerMenuButton.textContent = 'MENU';
+
+    header.append(headerHomeButton, headerMenuButton, headerAboutButton);
+
+    content.appendChild(header);
+}
+
+createHeaderBar();
