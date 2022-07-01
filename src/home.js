12e586256ee252b9d449d732a2224a67528106e3
Liam Birkinshaw-Doyle <ldoyle369@gmail.com>
2022-07-01 11:08:37 +0100

    finish styling of section elements

--- src/home.js
+++ src/home.js
@@ -1,5 +1,4 @@
 import * as DOM from './dom.js';
-import foodicon from './images/salad-bowl.jpg';
 
 export function createHomePage () {
     DOM.createTitle('SpaceBar');
@@ -7,7 +6,5 @@ export function createHomePage () {
     DOM.createSection('Discover', 'Out of this world flavours will take you to the next level. Conveniently located in the heart of San Fransokyo, stop in before a visit to downtown or heading on to the Institute of Technology.');
     DOM.createSection('Hours', "Sunday-Thursday: 12:00 - 22:00", "Friday-Saturday: 11:00 - 23:00");
     DOM.createSection('Find Us', '319 Moonrise Plaza,', 'San Fransokyo,', 'CA 94114,', 'United States of America')
-
-    DOM.createPictureSection('Menu Item', foodicon, 'Try some of our delicious food!')
 
 }
